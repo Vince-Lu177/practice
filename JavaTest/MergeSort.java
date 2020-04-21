92ccf7a2c033e163f5f677d0dd63bc2b40ae8d8a
Vince-Lu177 <vince2632469293@163.com>
2020-04-21 22:28:36 +0800

    Today's Practice

---
+++ JavaTest/MergeSort.java
@@ -0,0 +1,13 @@
+package Practice_0419;
+
+public class MergeSort {
+    public static void mergeSort(int[] array,int low,int mid,int high){
+        //把有序区间[low,mid),[mid,high)合并成一个有序区间
+        int[] output = new int[high - low];
+        int outputIndex = 0;
+        //记录output数组中元素存放个数
+        int cur1 = low;
+        int cur2 = mid;
+    }
+
+}
